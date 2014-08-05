3b566a84546a06d468d77e0d26dfec7e8ae132ba
Your Name <pillows.hf@gmail.com>
2014-08-05 18:13:53 +0000

    Fixed bugs

--- encrypted_paste.py
+++ encrypted_paste.py
@@ -8,8 +8,8 @@ encrypted = Blueprint("encrypted",__name__)
 @encrypted.route("/encrypted/<uid>",methods=['GET','POST'])
 def encrypted_(uid):
     page = "encrypted"
-    paste = config.db.pastes.find_one({"id":uid})
-    if not paste:
+    data = config.db.pastes.find_one({"id":uid})
+    if not data:
         return redirect("/")
     return render_template("post.html",page=page, data=data)
 
