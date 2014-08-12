e4d6847ccc73b1b34086f1b7cc48dd5375fff8a1
Your Name <pillows.hf@gmail.com>
2014-08-12 22:04:05 +0000

    Frankie you're going to have to import a lot of atuff

--- post.py
+++ post.py
@@ -5,18 +5,18 @@ from pygments.formatters import HtmlFormatter
 import config
 post=Blueprint("post",__name__)
 
+		
 #Eventually I'm going to have to change this to affect the GET request that should be generated for the AES key.
 @post.route("/paste/<uid>",methods=['GET'])
 def post_(uid):
     site=config.site
     page="Paste"
     data = config.db.pastes.find_one({"id":uid})
-    print data['lang']
-    data['paste']=Markup(highlight(data['paste'], get_lexer_by_name(data['lang']), HtmlFormatter(linenos=True)))
+    #data['paste']=Markup(highlight(data['paste'],get_lexer_by_name(data['lang'], encoding='UTF-8'),HtmlFormatter(linenos='table',anchorlinenos=True,lineanchors='L',linespans='L')))
     if not data:
-        return redirect("/")
+		return redirect("/")
     if data['encrypted']:
-        return redirect("/encrypted/{0}".format(uid))
+		return redirect("/encrypted/{0}".format(uid))
     elif data['oneview']:
-        config.db.pastes.remove({"id":uid})
+		config.db.pastes.remove({"id":uid})
     return render_template("post.html",page=page,site=site, data=data)
