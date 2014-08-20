57c41fd3c009060b684b87a5316cc204fc6dfac1
Pillows <matthew.wong@outlook.com>
2014-08-19 21:41:27 -0400

    Added public/private listing for pastes

--- index.py
+++ index.py
@@ -16,32 +16,37 @@ def index_():
     edition=config.edition
 
     page="Index"
-    latest = config.db.pastes.find().sort("_id",-1).limit(10)
+    latest = config.db.pastes.find({"display":"public"}).sort("_id",-1).limit(10)
     if request.method == "POST":
-		title = request.form['title']
-		paste = request.form['paste']
-		expiration = request.form['expiration']
-		encryption = request.form['encryption']
-		oneview = False#request.form["oneview"]
-		language = request.form['lang']
-		#print language
-		#print oneview
-		id_ = uuid.uuid4().hex
-                if encryption:
-                    encrypted = True
-                    encryption = hashlib.md5(encryption).hexdigest()
-                    paste = base64.b64encode(aes.encryptData(encryption, paste))
-                else:
-                    encrypted = False
-                if oneview:
-                    oneview = True
-                else:
-                    oneview = False
-                if expiration:
-                    expiration = True
-                else:
-                    expiration = False
+        title = request.form['title']
+        paste = request.form['paste']
+        expiration = request.form['expiration']
+        encryption = request.form['encryption']
+        oneview = request.form["oneview"]
+        language = request.form['lang']
+        display = request.form['displays']
+        #print language
+        #print oneview
+        id_ = uuid.uuid4().hex
 
-                config.db.pastes.insert({"paste":paste, "id":id_, "title":title, "encrypted":encrypted, "password":encryption, "oneview":oneview, "lang":language, "tag":language, "expiration":expiration, "time":time.time()})
-		return redirect("/paste/{0}".format(id_))
-    return render_template("index.html",page=page, site=site, hexid=hexid, serverid=serverid, edition=edition latest=latest)
+        if encryption:
+            encrypted = True
+            encryption = hashlib.md5(encryption).hexdigest()
+            paste = base64.b64encode(aes.encryptData(encryption, paste))
+        else:
+            encrypted = False
+        if oneview:
+            oneview = True
+        else:
+            oneview = False
+        if expiration:
+            expiration = True
+        else:
+            expiration = False
+
+        if title == "":
+            title = "Untitled"
+
+        config.db.pastes.insert({"paste":paste, "id":id_, "title":title, "encrypted":encrypted, "password":encryption, "oneview":oneview, "lang":language, "tag":language, "expiration":expiration, "time":time.time(), "display":display})
+        return redirect("/paste/{0}".format(id_))
+    return render_template("index.html",page=page, site=site, hexid=hexid, serverid=serverid, edition=edition, latest=latest)
